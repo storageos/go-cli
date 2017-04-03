4ba2b268238140b0bb045a8a2653a6bb4806d23c
Cheryl Hung <cheryl.hung@storageos.com>
2017-04-03 13:24:03 +0100

    Add beta warning and nagware to CLI

--- cmd/storageos/storageos.go
+++ cmd/storageos/storageos.go
@@ -28,7 +28,7 @@ func newStorageOSCommand(storageosCli *command.StorageOSCli) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:              "storageos [OPTIONS] COMMAND [ARG...]",
 		Short:            "Converged storage for containers",
-		Long:             "By using this product, you are agreeing to the terms of the the StorageOS Ltd. End User Subscription Agreement (EUSA) found at: http://storageos.com/legal/#eusa",
+		Long:             ,
 		SilenceUsage:     true,
 		SilenceErrors:    true,
 		TraverseChildren: true,
@@ -295,3 +295,12 @@ func hasTags(cmd *cobra.Command) bool {
 
 	return false
 }
+
+var legalDisclaimer = `
+By using this product, you are agreeing to the terms of the the StorageOS Ltd. End User Subscription Agreement (EUSA) found at: http://storageos.com/legal/#eusa
+`
+
+var betaWarning = `
+WARNING: This is the beta version of StorageOS and may stop functioning with no notice.
+To be notified about stable releases and latest features, sign up at my.storageos.com.
+`
