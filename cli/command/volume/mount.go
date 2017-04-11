3994649a8d714e1af27c0ade138155e9e33c9552
Karolis Rusenas <karolis.rusenas@gmail.com>
2017-04-11 14:24:17 +0100

    further work on mounting the volume

--- cli/command/volume/mount.go
+++ cli/command/volume/mount.go
@@ -1,37 +1,48 @@
 package volume
 
 import (
+	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/dnephin/cobra"
 	storageos "github.com/storageos/go-api"
 	"github.com/storageos/go-cli/cli"
 	"github.com/storageos/go-cli/cli/command"
 	cliconfig "github.com/storageos/go-cli/cli/config"
+	"github.com/storageos/go-cli/pkg/host"
+	"github.com/storageos/go-cli/pkg/mount"
 	"github.com/storageos/go-cli/pkg/system"
+	"github.com/storageos/go-cli/pkg/validation"
+
+	"github.com/storageos/go-api/types"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 type mountOptions struct {
 	ref        string
 	mountpoint string // mountpoint
+	fsType     string
 }
 
 func newMountCommand(storageosCli *command.StorageOSCli) *cobra.Command {
 	var opt mountOptions
 
 	cmd := &cobra.Command{
-		Use:   "mount [OPTIONS] VOLUME",
+		Use:   "mount [OPTIONS] VOLUME MOUNTPOINT",
 		Short: "Mount specified volume",
-		Args:  cli.ExactArgs(1),
+		Args:  cli.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opt.ref = args[0]
+			opt.mountpoint = args[1]
 			return runMount(storageosCli, opt)
 		},
 	}
 
 	flags := cmd.Flags()
-	flags.StringVarP(&opt.mountpoint, "mountpoint", "m", "", `Volume mountpoint`)
+	flags.StringVarP(&opt.fsType, "fsType", "m", cliconfig.DefaultFSType, `Volume mountpoint`)
 
 	return cmd
 }
@@ -44,30 +55,108 @@ func runMount(storageosCli *command.StorageOSCli, opt mountOptions) error {
 		return fmt.Errorf("device root path '%s' not found, check whether StorageOS is running", cliconfig.DeviceRootPath)
 	}
 
-	fmt.Println("checking volume ", opt.ref, opt.mountpoint)
+	// fmt.Println("checking volume ", opt.ref, opt.mountpoint)
 
-	errs := isVolumeReady(storageosCli, opt.ref)
+	// validating fsType
+	err = validation.IsValidFSType(opt.fsType)
+	if err != nil {
+		return err
+	}
+
+	client := storageosCli.Client()
+	namespace, name, err := storageos.ParseRef(opt.ref)
+	if err != nil {
+		return err
+	}
+
+	log.WithFields(log.Fields{
+		"namespace":  namespace,
+		"volumeName": name,
+	}).Info("parsed volume ref")
+
+	vol, err := client.Volume(namespace, name)
+	if err != nil {
+		return err
+	}
+
+	// checking readiness
+	errs := isVolumeReady(vol, name)
 	if len(errs) > 0 {
 		return fmt.Errorf("cannot mount volume: %s", strings.Join(errs, ", "))
 	}
 
+	var hostname string
+
+	// getting current hostname
+	hostname, err = host.Get()
+	if err != nil {
+		hostname = "unknown"
+	}
+
 	fmt.Println("volume is ready for mount, mounting..")
+	err = client.VolumeMount(types.VolumeMountOptions{ID: vol.ID, Namespace: namespace, Client: hostname})
+	if err != nil {
+		return err
+	}
+
+	err = retryableMount(vol, opt.mountpoint, opt.fsType)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"namespace":  namespace,
+			"volumeName": name,
+			"error":      err,
+		}).Error("error while mounting volume")
+		// should unmount volume in the CP if we failed here
+		newErr := client.VolumeUnmount(types.VolumeUnmountOptions{ID: vol.ID, Namespace: namespace})
+		if newErr != nil {
+			log.WithFields(log.Fields{
+				"volumeId": vol.ID,
+				"err":      newErr,
+			}).Error("failed to unmount volume")
+		}
+
+		return err
+	}
+
+	fmt.Println("volume mounted")
+
+	return nil
+}
+
+func retryableMount(volume *types.Volume, mountpoint, fsType string) error {
+
+	driver := mount.New(cliconfig.DeviceRootPath)
+
+	// unmount can take some time
+	maxRetries := 10
+	retries := 0
+
+RETRY:
+	// Perform the mount
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	err := driver.MountVolume(ctx, volume.ID, mountpoint, fsType)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"volume_id":  volume.ID,
+			"mountpoint": mountpoint,
+			"err":        err.Error(),
+		}).Error(" failed to mount volume")
+
+		if retries < maxRetries {
+			time.Sleep(250 * time.Millisecond)
+			retries++
+			goto RETRY
+		}
+
+		return err
+	}
 
 	return nil
 }
 
 // isVolumeReady - mount only unmounted and active volume
-func isVolumeReady(storageosCli *command.StorageOSCli, ref string) (errs []string) {
-	client := storageosCli.Client()
-
-	namespace, name, err := storageos.ParseRef(ref)
-	if err != nil {
-		return []string{err.Error()}
-	}
-	vol, err := client.Volume(namespace, name)
-	if err != nil {
-		return []string{err.Error()}
-	}
+func isVolumeReady(vol *types.Volume, ref string) (errs []string) {
 
 	if vol.Status != "active" {
 		errs = append(errs, fmt.Sprintf("can only mount active volumes, current status: '%s'", vol.Status))
