404717579b971adc0d1ec292f429c84d4b0bd42d
Joe Reid <joe.reid@storageos.com>
2017-08-07 12:31:23 +0100

    Update cluster health cp/dp options to be flags

--- cli/command/cluster/health.go
+++ cli/command/cluster/health.go
@@ -14,15 +14,36 @@ import (
 	cliTypes "github.com/storageos/go-cli/types"
 )
 
+type healthOpt struct {
+	cpHealth bool
+	dpHealth bool
+	cluster  string
+}
+
+func (h healthOpt) cp() bool {
+	return h.cpHealth || !(h.cpHealth || h.dpHealth)
+}
+
+func (h healthOpt) dp() bool {
+	return h.dpHealth || !(h.dpHealth || h.cpHealth)
+}
+
 func newHealthCommand(storageosCli *command.StorageOSCli) *cobra.Command {
+	opt := &healthOpt{}
+
 	cmd := &cobra.Command{
-		Use:   "health cp|dp CLUSTER_TOKEN",
+		Use:   "health [--cp | --dp] CLUSTER_TOKEN",
 		Short: `Displays the cluster's health information from a cluster token (as given by cluster create)`,
-		Args:  cli.ExactArgs(2),
+		Args:  cli.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runHealth(storageosCli, args[1], args[0])
+			opt.cluster = args[0]
+			return runHealth(storageosCli, opt)
 		},
 	}
+
+	flag := cmd.Flags()
+	flag.BoolVar(&opt.cpHealth, "cp", false, "Display the output from the control plane only")
+	flag.BoolVar(&opt.dpHealth, "dp", false, "Display the output from the data plane only")
 
 	return cmd
 }
@@ -73,23 +94,31 @@ func runDPHealth(storageosCli *command.StorageOSCli, cluster *cliTypes.Cluster) 
 	}, clusterHealth)
 }
 
-func runHealth(storageosCli *command.StorageOSCli, clusterID string, cpdp string) error {
+func runHealth(storageosCli *command.StorageOSCli, opt *healthOpt) error {
 	client, err := discovery.NewClient("", "", "")
 	if err != nil {
 		return err
 	}
 
-	cluster, err := client.ClusterStatus(clusterID)
+	cluster, err := client.ClusterStatus(opt.cluster)
 	if err != nil {
 		return err
 	}
 
-	if cpdp == "cp" {
+	switch {
+	case opt.cp() && opt.dp():
+		fmt.Fprintln(storageosCli.Out(), "Controlplane:")
+		if err := runCPHealth(storageosCli, cluster); err != nil {
+			return err
+		}
+
+		fmt.Fprintln(storageosCli.Out(), "\nDataplane:")
+		return runDPHealth(storageosCli, cluster)
+
+	case opt.cp():
 		return runCPHealth(storageosCli, cluster)
-	}
-	if cpdp == "dp" {
+
+	default:
 		return runDPHealth(storageosCli, cluster)
 	}
-
-	return fmt.Errorf("Unknown instance type selector: %s (expecting [cp|dp])", cpdp)
 }
