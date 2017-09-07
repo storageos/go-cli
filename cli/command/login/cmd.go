68395f84da37b75ab0cc19537c45458138b26c8f
Joe Reid <joe.reid@storageos.com>
2017-09-07 14:58:05 +0100

    Add new login CLI endpoint

---
+++ cli/command/login/cmd.go
@@ -0,0 +1,110 @@
+package login
+
+import (
+	"errors"
+	"github.com/dnephin/cobra"
+	"os"
+
+	"github.com/storageos/go-cli/cli"
+	"github.com/storageos/go-cli/cli/command"
+	"github.com/storageos/go-cli/cli/config"
+)
+
+type loginOptions struct {
+	host     string
+	username string
+	password string
+	delete   bool
+}
+
+func NewLoginCommand(storageosCli *command.StorageOSCli) *cobra.Command {
+	opt := loginOptions{}
+
+	cmd := &cobra.Command{
+		Use:   "login [HOST]",
+		Short: "Store login credentials for a given storageos host",
+		Args:  cli.RequiresMaxArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if opt.delete {
+				return runDelete(storageosCli, opt, args)
+			}
+			return runLogin(storageosCli, opt, args)
+		},
+	}
+
+	flags := cmd.Flags()
+	flags.StringVar(&opt.host, "host", "", "The host to store the credentials for")
+	flags.Lookup("host").Hidden = true
+	flags.StringVar(&opt.username, "username", "", "The username to use for this host")
+	flags.StringVar(&opt.password, "password", "", "The username to use for this host")
+	flags.BoolVar(&opt.delete, "d", false, "Delete entry for this host")
+
+	return cmd
+}
+
+func getHost(opt loginOptions, args []string) (string, error) {
+	var host string
+
+	switch {
+	case len(args) == 1:
+		if opt.host != "" {
+			return "", errors.New("Conflicting options: either specify --host or provide positional arg, not both")
+		}
+		host = args[0]
+
+	case opt.host != "":
+		host = opt.host
+
+	default:
+		host = os.Getenv(config.EnvStorageOSHost)
+		if host == "" {
+			return "", errors.New("No setting found for host")
+		}
+
+	}
+
+	return host, nil
+}
+
+func runLogin(storageosCli *command.StorageOSCli, opt loginOptions, args []string) error {
+	host, err := getHost(opt, args)
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case opt.username == "":
+		return errors.New("Please provide a --username")
+
+	case opt.password == "":
+		return errors.New("Please provide a --password")
+
+	default:
+		err := storageosCli.ConfigFile().CredentialsStore.SetCredetials(host, opt.username, opt.password)
+		if err != nil {
+			return err
+		}
+
+		return storageosCli.ConfigFile().Save()
+	}
+}
+
+func runDelete(storageosCli *command.StorageOSCli, opt loginOptions, args []string) error {
+	host, err := getHost(opt, args)
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case opt.username != "":
+		return errors.New("Do not provide --username when deleting credentials")
+
+	case opt.password != "":
+		return errors.New("Do not provide --password when deleting credentials")
+
+	default:
+		storageosCli.ConfigFile().CredentialsStore.DeleteCredetials(host)
+		return nil
+	}
+
+}
