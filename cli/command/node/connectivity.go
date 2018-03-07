37ed517b102fbc2b125fecb1860ff6e8866879dd
J.C.Reid <jcr32@kent.ac.uk>
2018-03-07 11:33:41 +0000

    wip changes for connectivity check

---
+++ cli/command/node/connectivity.go
@@ -0,0 +1,70 @@
+package node
+
+import (
+	"fmt"
+	"github.com/dnephin/cobra"
+	"io"
+	// storageos "github.com/storageos/go-api"
+	"github.com/storageos/go-api/types"
+	"github.com/storageos/go-cli/cli"
+	"github.com/storageos/go-cli/cli/command"
+	"github.com/storageos/go-cli/cli/command/formatter"
+	"github.com/storageos/go-cli/cli/command/inspect"
+)
+
+type inspectOptions struct {
+	format string
+	names  []string
+}
+
+func newConnectivityCommand(storageosCli *command.StorageOSCli) *cobra.Command {
+	var opt inspectOptions
+
+	cmd := &cobra.Command{
+		Use:   "connectivity [OPTIONS] NODE [NODE...]",
+		Short: "Display detailed connectivity information on one or more nodes",
+		Args:  cli.RequiresMinArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opt.names = args
+			return runConnectivity(storageosCli, opt)
+		},
+	}
+
+	cmd.Flags().StringVarP(&opt.format, "format", "f", "", "Format the output using the given Go template")
+	return cmd
+}
+
+func runConnectivity(storageosCli *command.StorageOSCli, opt inspectOptions) error {
+	client := storageosCli.Client()
+
+	result := make(map[string]types.NodeConnectivity)
+	for _, ref := range opt.names {
+		c, err := client.Connectivity(ref)
+		if err != nil {
+			return err
+		}
+
+		result[ref] = c
+	}
+
+	return printConnectivityResult(storageosCli.Out(), result)
+}
+
+func printConnectivityResult(out io.Writer, result map[string]types.NodeConnectivity) error {
+	var printRefHeader = len(result) > 1
+
+	for node, result := range result {
+		if printRefHeader {
+			fmt.Fprintf(out, "%s:\n", node)
+		}
+
+		fmtCtx := formatter.Context{
+			Output: storageosCli.Out(),
+			Format: formatter.newConnectivityFormat(formatter.TableFormatKey),
+		}
+
+		if err := formatter.ConnectivityWrite(nodeCtx, nodes); err != nil {
+			return error
+		}
+	}
+}
