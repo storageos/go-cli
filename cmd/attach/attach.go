3afce1aa1612a723204ae580e05c62fb14399d92
Fraser Savage <fraser.savage@storageos.com>
2020-03-24 09:30:13 +0000

    Merge pull request #35 in STORAGEOS/c2-cli from feature/CP-4161-cli-remove-redundant-volume-in-attach-volume to master
    Squashed commit of the following:
    
    commit 4eabb12e7a1cb951d4f9a7c3de421695778ecab4
    Author: Fraser Savage <fraser.savage@storageos.com>
    Date:   Mon Mar 23 17:39:45 2020 +0000
    
        cmd: Remove redundant volume subcommand for attach and detach

--- cmd/attach/attach.go
+++ cmd/attach/attach.go
@@ -2,12 +2,15 @@ package attach
 
 import (
 	"context"
+	"errors"
 	"io"
 	"os"
 	"time"
 
 	"github.com/spf13/cobra"
 
+	"code.storageos.net/storageos/c2-cli/cmd/argwrappers"
+	"code.storageos.net/storageos/c2-cli/cmd/runwrappers"
 	"code.storageos.net/storageos/c2-cli/namespace"
 	"code.storageos.net/storageos/c2-cli/node"
 	"code.storageos.net/storageos/c2-cli/output"
@@ -16,6 +19,11 @@ import (
 	"code.storageos.net/storageos/c2-cli/output/yamlformat"
 	"code.storageos.net/storageos/c2-cli/pkg/id"
 	"code.storageos.net/storageos/c2-cli/volume"
+)
+
+var (
+	errArguments            = errors.New("must specify exactly two arguments <volume> <node>")
+	errNoNamespaceSpecified = errors.New("must specify the namespace of the volume to attach")
 )
 
 // ConfigProvider specifies the configuration settings which commands require
@@ -42,19 +50,112 @@ type Displayer interface {
 	AttachVolume(ctx context.Context, w io.Writer) error
 }
 
-// NewCommand configures the set of commands which are grouped by the "attach"
-// verb.
-func NewCommand(client Client, config ConfigProvider) *cobra.Command {
-	command := &cobra.Command{
-		Use:   "attach",
-		Short: "Attach volumes to nodes",
+type attachCommand struct {
+	config  ConfigProvider
+	client  Client
+	display Displayer
+
+	namespace string
+
+	writer io.Writer
+}
+
+func (c *attachCommand) runWithCtx(ctx context.Context, cmd *cobra.Command, args []string) error {
+	useIDs, err := c.config.UseIDs()
+	if err != nil {
+		return err
 	}
 
-	command.AddCommand(
-		newVolume(os.Stdout, client, config),
+	var (
+		namespaceID id.Namespace
+		volumeID    id.Volume
+		nodeID      id.Node
 	)
 
-	return command
+	if useIDs {
+		namespaceID = id.Namespace(c.namespace)
+		volumeID = id.Volume(args[0])
+		nodeID = id.Node(args[1])
+	} else {
+		ns, err := c.client.GetNamespaceByName(ctx, c.namespace)
+		if err != nil {
+			return err
+		}
+		namespaceID = ns.ID
+
+		volName := args[0]
+		vol, err := c.client.GetVolumeByName(ctx, namespaceID, volName)
+		if err != nil {
+			return err
+		}
+		volumeID = vol.ID
+
+		nodeName := args[1]
+		node, err := c.client.GetNodeByName(ctx, nodeName)
+		if err != nil {
+			return err
+		}
+		nodeID = node.ID
+	}
+
+	err = c.client.AttachVolume(ctx, namespaceID, volumeID, nodeID)
+	if err != nil {
+		return err
+	}
+
+	return c.display.AttachVolume(ctx, c.writer)
+}
+
+// NewCommand configures the "attach" verb command.
+func NewCommand(client Client, config ConfigProvider) *cobra.Command {
+	c := &attachCommand{
+		config: config,
+		client: client,
+		writer: os.Stdout,
+	}
+
+	cobraCommand := &cobra.Command{
+		Use:   "attach",
+		Short: "Attach a volume to a node",
+		Example: `
+$ storageos attach volume --namespace my-namespace-name my-volume my-node
+`,
+
+		Args: argwrappers.WrapInvalidArgsError(func(cmd *cobra.Command, args []string) error {
+			if len(args) != 2 {
+				return errArguments
+			}
+			return nil
+		}),
+		PreRunE: argwrappers.WrapInvalidArgsError(func(_ *cobra.Command, _ []string) error {
+			c.display = SelectDisplayer(c.config)
+
+			ns, err := c.config.Namespace()
+			if err != nil {
+				return err
+			}
+
+			if ns == "" {
+				return errNoNamespaceSpecified
+			}
+			c.namespace = ns
+
+			return nil
+		}),
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			run := runwrappers.Chain(
+				runwrappers.RunWithTimeout(c.config),
+				runwrappers.EnsureNamespaceSetWhenUseIDs(c.config),
+			)(c.runWithCtx)
+
+			return run(context.Background(), cmd, args)
+		},
+
+		SilenceUsage: true,
+	}
+
+	return cobraCommand
 }
 
 // SelectDisplayer returns the right command displayer specified in the
--- cmd/detach/detach.go
+++ cmd/detach/detach.go
@@ -2,6 +2,7 @@ package detach
 
 import (
 	"context"
+	"errors"
 	"io"
 	"os"
 	"time"
@@ -9,6 +10,9 @@ import (
 	"github.com/spf13/cobra"
 
 	"code.storageos.net/storageos/c2-cli/apiclient"
+	"code.storageos.net/storageos/c2-cli/cmd/argwrappers"
+	"code.storageos.net/storageos/c2-cli/cmd/flagutil"
+	"code.storageos.net/storageos/c2-cli/cmd/runwrappers"
 	"code.storageos.net/storageos/c2-cli/namespace"
 	"code.storageos.net/storageos/c2-cli/node"
 	"code.storageos.net/storageos/c2-cli/output"
@@ -16,7 +20,13 @@ import (
 	"code.storageos.net/storageos/c2-cli/output/textformat"
 	"code.storageos.net/storageos/c2-cli/output/yamlformat"
 	"code.storageos.net/storageos/c2-cli/pkg/id"
+	"code.storageos.net/storageos/c2-cli/pkg/version"
 	"code.storageos.net/storageos/c2-cli/volume"
+)
+
+var (
+	errArguments            = errors.New("must specify exactly one volume to detach")
+	errNoNamespaceSpecified = errors.New("must specify the namespace of the volume to detach")
 )
 
 // ConfigProvider specifies the configuration settings which commands require
@@ -43,19 +53,116 @@ type Displayer interface {
 	DetachVolume(ctx context.Context, w io.Writer) error
 }
 
-// NewCommand configures the set of commands which are grouped by the "detach"
-// verb.
-func NewCommand(client Client, config ConfigProvider) *cobra.Command {
-	command := &cobra.Command{
-		Use:   "detach",
-		Short: "Detach volumes from nodes",
+type detachCommand struct {
+	config  ConfigProvider
+	client  Client
+	display Displayer
+
+	namespace string
+
+	// useCAS determines whether the command makes the detach request
+	// constrained by the provided casVersion.
+	useCAS     func() bool
+	casVersion string
+
+	writer io.Writer
+}
+
+func (c *detachCommand) runWithCtx(ctx context.Context, cmd *cobra.Command, args []string) error {
+	useIDs, err := c.config.UseIDs()
+	if err != nil {
+		return err
 	}
 
-	command.AddCommand(
-		newVolume(os.Stdout, client, config),
-	)
+	namespaceID := id.Namespace(c.namespace)
+	volumeID := id.Volume(args[0])
 
-	return command
+	// If not using IDs, get the appropriate IDs
+	if !useIDs {
+		ns, err := c.client.GetNamespaceByName(ctx, c.namespace)
+		if err != nil {
+			return err
+		}
+		namespaceID = ns.ID
+
+		volName := args[0]
+		vol, err := c.client.GetVolumeByName(ctx, namespaceID, volName)
+		if err != nil {
+			return err
+		}
+		volumeID = vol.ID
+	}
+
+	params := &apiclient.DetachVolumeRequestParams{}
+	if c.useCAS() {
+		params.CASVersion = version.FromString(c.casVersion)
+	}
+
+	err = c.client.DetachVolume(
+		ctx,
+		namespaceID,
+		volumeID,
+		params,
+	)
+	if err != nil {
+		return err
+	}
+
+	return c.display.DetachVolume(ctx, c.writer)
+}
+
+// NewCommand configures the "detach" verb command.
+func NewCommand(client Client, config ConfigProvider) *cobra.Command {
+	c := &detachCommand{
+		config: config,
+		client: client,
+		writer: os.Stdout,
+	}
+
+	cobraCommand := &cobra.Command{
+		Use:   "detach",
+		Short: "Detach a volume from its current location",
+		Example: `
+$ storageos detach volume --namespace my-namespace-name my-volume
+`,
+
+		Args: argwrappers.WrapInvalidArgsError(func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errArguments
+			}
+			return nil
+		}),
+		PreRunE: argwrappers.WrapInvalidArgsError(func(_ *cobra.Command, _ []string) error {
+			c.display = SelectDisplayer(c.config)
+
+			ns, err := c.config.Namespace()
+			if err != nil {
+				return err
+			}
+
+			if ns == "" {
+				return errNoNamespaceSpecified
+			}
+			c.namespace = ns
+
+			return nil
+		}),
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			run := runwrappers.Chain(
+				runwrappers.RunWithTimeout(c.config),
+				runwrappers.EnsureNamespaceSetWhenUseIDs(c.config),
+			)(c.runWithCtx)
+
+			return run(context.Background(), cmd, args)
+		},
+
+		SilenceUsage: true,
+	}
+
+	c.useCAS = flagutil.SupportCAS(cobraCommand.Flags(), &c.casVersion)
+
+	return cobraCommand
 }
 
 // SelectDisplayer returns the right command displayer specified in the
